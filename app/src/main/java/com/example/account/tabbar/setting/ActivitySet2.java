c6e9d38946e8d69db788693b95f279a506aab937
ymj <1239604213@qq.com>
2020-10-20 16:59:36 +0800

    nine

--- app/src/main/java/com/example/account/tabbar/setting/ActivitySet2.java
+++ app/src/main/java/com/example/account/tabbar/setting/ActivitySet2.java
@@ -2,7 +2,11 @@ package com.example.account.tabbar.setting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.example.account.R;
 
@@ -13,4 +17,30 @@ public class ActivitySet2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set2);
     }
+
+    public void onClick(View view) {
+        Intent intent = new Intent();
+        switch (view.getId()) {
+            //使用隐式Intent实现 Activity的跳转
+            case R.id.arrow:
+                intent.setAction("com.example.activity.SECOND_ACTIVITY");
+                intent.putExtra("msg","second activity");
+                break;
+        }
+        //判断intent 指向的action是否存在
+        if(intent.resolveActivity(getPackageManager()) !=null) {
+            startActivity(intent);
+        }
+    }
+
+    public void swtichClick(View view){
+        Switch sw = (Switch)view;
+        boolean isChecked = sw.isChecked();
+        if(isChecked){
+            Toast.makeText(ActivitySet2.this, "开启", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(ActivitySet2.this, "关闭", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
